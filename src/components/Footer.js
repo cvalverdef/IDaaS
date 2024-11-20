8f711d4f0d773611b5436c158039e3f239ce7607
Cristiano <developer@yashar.com.br>
2024-11-20 18:39:52 -0300

    f1rst

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-secondary p-4 text-center text-white">
+    © 2024 NeuroID. All Rights Reserved.
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => (
+  <nav className="bg-primary p-4">
+    <h1 className="text-white text-xl">NeuroID</h1>
+  </nav>
+);
+
+export default Navbar;
